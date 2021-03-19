a1ec5d57b60867ad67e7f5ffbcba9ce5e82be714
Maria Pia Gaudenzi <pgaudenzi@ar0c02dx29zml85.fibertel.com.ar>
2021-03-19 14:00:45 -0300

    adding fecha

--- clase2_java_tm/src/ej5/Fecha.java
+++ clase2_java_tm/src/ej5/Fecha.java
@@ -1,12 +1,36 @@
 package ej5;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class Fecha {
 
-    private GregorianCalendar gregorianCalendar;
+    final static int[] NORMAL = {31,28,31,30,31,30,31,31,30,31,30,31};
+    final static int[] BISIESTO = {31,29,31,30,31,30,31,31,30,31,30,31};
 
-    public Fecha() {
+    private Date date;
 
+    public Fecha(int dia, int mes, int anio) {
+        date = new Date(anio, mes, dia);
+    }
+
+    public void sumar() {
+        GregorianCalendar gregorianCalendar =
+                new GregorianCalendar(date.getYear(), date.getMonth(), date.getDay());
+        gregorianCalendar.add(GregorianCalendar.DATE, 1);
+        System.out.println(gregorianCalendar.toString());
+    }
+
+    public boolean esBisiesto() {
+        return new GregorianCalendar().isLeapYear(date.getYear());
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
 }
