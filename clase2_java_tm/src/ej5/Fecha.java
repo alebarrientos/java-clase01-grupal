3f40f0b37a9b988ca18b4cc92ecddacbead86b8e
WorxtenMeLi <jhon.pulidopena@mercadolibre.com.co>
2021-03-19 12:15:55 -0500

    Edite el ejercico 5 Agregando el checkDAte()

--- clase2_java_tm/src/ej5/Fecha.java
+++ clase2_java_tm/src/ej5/Fecha.java
@@ -6,8 +6,8 @@ import java.util.GregorianCalendar;
 
 public class Fecha {
 
-    final static int[] NORMAL = {31,28,31,30,31,30,31,31,30,31,30,31};
-    final static int[] BISIESTO = {31,29,31,30,31,30,31,31,30,31,30,31};
+    final static int[] NORMAL = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    final static int[] BISIESTO = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     private Date date;
 
@@ -26,11 +26,30 @@ public class Fecha {
         return new GregorianCalendar().isLeapYear(date.getYear());
     }
 
+    public static boolean esBisiesto(int anio) {
+        return new GregorianCalendar().isLeapYear(anio);
+    }
+
+
     public Date getDate() {
         return date;
     }
 
     public void setDate(Date date) {
         this.date = date;
+    }
+
+    public static boolean checkDate(int dia, int mes, int anio) {
+        if (dia > 31 || mes > 12)
+            return false;
+        int[] dias;
+        if (Fecha.esBisiesto(anio))
+            dias = NORMAL;
+        else
+            dias = BISIESTO;
+        if (dia < dias[mes - 1])
+            return true;
+        else
+            return false;
     }
 }
